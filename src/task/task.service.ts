ce912ad484510dedecf4a8848456d5930f3bb3e6
Amanuel Shiferaw <123aman64@gmail.com>
2024-03-09 19:19:57 +0300

    remove:spec files and add class validator for dto

--- src/task/task.service.ts
+++ src/task/task.service.ts
@@ -1,4 +1,33 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { Task } from './entities/task.entity';
 
 @Injectable()
-export class TaskService {}
+export class TaskService {
+  constructor(
+    @InjectRepository(Task)
+    private taskRepository: Repository<Task>,
+  ) {}
+
+  async findAll(): Promise<Task[]> {
+    return this.taskRepository.find();
+  }
+
+  async findOne(id: number): Promise<Task> {
+    return this.taskRepository.findOneBy({ id });
+  }
+
+  async create(task: Task): Promise<Task> {
+    return this.taskRepository.save(task);
+  }
+
+  async update(id: number, task: Task): Promise<Task> {
+    await this.taskRepository.update(id, task);
+    return this.taskRepository.findOneBy({ id });
+  }
+
+  async delete(id: number): Promise<void> {
+    this.taskRepository.delete(id);
+  }
+}
--- src/todo/todo.service.ts
+++ src/todo/todo.service.ts
@@ -1,4 +1,33 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Todo } from './entities/todo.entity';
+import { Repository } from 'typeorm';
 
 @Injectable()
-export class TodoService {}
+export class TodoService {
+  constructor(
+    @InjectRepository(Todo)
+    private todoRepository: Repository<Todo>,
+  ) {}
+
+  async findAll(): Promise<Todo[]> {
+    return this.todoRepository.find();
+  }
+
+  async findOne(id: number): Promise<Todo> {
+    return this.todoRepository.findOneBy({ id });
+  }
+
+  async create(todo: Todo): Promise<Todo> {
+    return this.todoRepository.save(todo);
+  }
+
+  async update(id: number, todo: Todo): Promise<Todo> {
+    await this.todoRepository.update(id, todo);
+    return this.todoRepository.findOneBy({ id });
+  }
+
+  async delete(id: number): Promise<void> {
+    this.todoRepository.delete(id);
+  }
+}
